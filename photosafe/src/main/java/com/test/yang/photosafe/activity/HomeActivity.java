96b5aaf9bf75035fb697b0a7ef504e6ed99a7e38
Yang <769053641@qq.com>
2016-05-08 13:26:53 +0800

    增加设置密码功能

--- photosafe/src/main/java/com/test/yang/photosafe/activity/HomeActivity.java
+++ photosafe/src/main/java/com/test/yang/photosafe/activity/HomeActivity.java
@@ -1,14 +1,22 @@
 package com.test.yang.photosafe.activity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.test.yang.photosafe.R;
 import com.test.yang.photosafe.adapters.HomeGridViewAdapter;
+import com.test.yang.photosafe.tools.MD5Tools;
 
 /**这是主界面
  * Created by Administrator on 2016/5/4.
@@ -16,11 +24,16 @@ import com.test.yang.photosafe.adapters.HomeGridViewAdapter;
 public class HomeActivity extends AppCompatActivity {
 
     private GridView mHomeGridView;
+    private int mSetPasswordSeeCount;
+    private int mInputPasswordSeeCount;
+    private AlertDialog alertDialog;
+    private SharedPreferences sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        sp=getSharedPreferences("config",MODE_PRIVATE);
         mHomeGridView= (GridView) findViewById(R.id.home_grid_view);
         mHomeGridView.setAdapter(new HomeGridViewAdapter(getApplicationContext()));
         mHomeGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -28,6 +41,11 @@ public class HomeActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 switch (position){
                     case 0:
+                        if(TextUtils.isEmpty(sp.getString("password",""))){
+                            showSetPasswordDialog();
+                        }else{
+                            showInputPasswordDialog();
+                        }
                         break;
                     case 1:
                         break;
@@ -53,5 +71,133 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
+    }
+    /**
+     * 建立设置密码对话框
+     */
+    protected void showSetPasswordDialog(){
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        View view=View.inflate(getApplicationContext(),R.layout.safe_set_password,null);
+        final EditText etSetPassword= (EditText) view.findViewById(R.id.set_password);
+        final EditText etAffirmPassword= (EditText) view.findViewById(R.id.affirm_password);
+        Button btCancel=(Button)view.findViewById(R.id.cancel_button);
+        Button btEnsure= (Button) view.findViewById(R.id.ensure_button);
+        ImageView ivSetPasswordSee= (ImageView) view.findViewById(R.id.set_passoword_see);
+        ImageView ivAffirmPasswordSee= (ImageView) view.findViewById(R.id.affirm_password_see);
+        ivSetPasswordSee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mSetPasswordSeeCount%2==1){
+                    //显示密码
+                    etSetPassword.setInputType(2);
+                    etAffirmPassword.setInputType(2);
+                }else{
+                    //隐藏密码
+                    etSetPassword.setInputType(129);
+                    etAffirmPassword.setInputType(129);
+                }
+                mSetPasswordSeeCount++;
+            }
+        });
+        ivAffirmPasswordSee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mSetPasswordSeeCount%2==1){
+                    //显示密码
+                    etSetPassword.setInputType(2);
+                    etAffirmPassword.setInputType(2);
+                }else{
+                    //隐藏密码
+                    etSetPassword.setInputType(129);
+                    etAffirmPassword.setInputType(129);
+                }
+                mSetPasswordSeeCount++;
+            }
+        });
+        btEnsure.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //得到一个去除空格的字符串
+                String password=etSetPassword.getText().toString().trim();
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(getApplicationContext(),"请输入密码",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                String affirmPassword=etAffirmPassword.getText().toString().trim();
+                if(password.equals(affirmPassword)){
+                    SharedPreferences.Editor editor=sp.edit();
+                    editor.putString("password", MD5Tools.passwordMD5(password));
+                    editor.commit();
+                    alertDialog.dismiss();
+                    Toast.makeText(getApplicationContext(),"密码设置成功",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(),"两次密码输入不同",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        btCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+            }
+        });
+        builder.setView(view);
+        alertDialog=builder.create();
+        alertDialog.show();
+
+    }
+    /**
+     * 建立输入密码对话框
+     */
+    protected void showInputPasswordDialog(){
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        View view=View.inflate(getApplicationContext(),R.layout.safe_input_password,null);
+        final EditText etInputPassword= (EditText) view.findViewById(R.id.affirm_password);
+        Button btInputCancel=(Button)view.findViewById(R.id.cancel_button);
+        Button btInputEnsure= (Button) view.findViewById(R.id.ensure_button);
+        ImageView ivInputPassword= (ImageView) view.findViewById(R.id.input_password_see);
+        ivInputPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mInputPasswordSeeCount%2==1){
+                    //显示密码
+                    etInputPassword.setInputType(0);
+                }else{
+                    //隐藏密码
+                    etInputPassword.setInputType(129);
+                }
+                mInputPasswordSeeCount++;
+            }
+        });
+        btInputEnsure.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputPassword=etInputPassword.getText().toString().trim();
+                if(TextUtils.isEmpty(inputPassword)){
+                    Toast.makeText(getApplicationContext(),"请输入密码",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                String savePassword=sp.getString("password","");
+                if(MD5Tools.passwordMD5(inputPassword).equals(savePassword)){
+                    alertDialog.dismiss();
+                    Toast.makeText(getApplicationContext(),"密码正确",Toast.LENGTH_SHORT).show();
+                    return;
+                }else{
+                    Toast.makeText(getApplicationContext(),"密码不正确",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        });
+        btInputCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+            }
+        });
+        builder.setView(view);
+        alertDialog=builder.create();
+        alertDialog.show();
     }
 }
---
+++ photosafe/src/main/java/com/test/yang/photosafe/tools/MD5Tools.java
@@ -0,0 +1,29 @@
+package com.test.yang.photosafe.tools;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by Administrator on 2016/5/8.
+ */
+public class MD5Tools {
+    public  static String passwordMD5(String password){
+        StringBuilder sb=new StringBuilder();
+        try {
+            MessageDigest messageDigest=MessageDigest.getInstance("MD5");
+            byte[] digest=messageDigest.digest(password.getBytes());
+            for(int i=0;i<digest.length;i++){
+                int result=digest[i];
+                String hexString=Integer.toHexString(result)+1;
+                if(hexString.length()<2){
+                    sb.append("0");
+                }
+                sb.append(hexString);
+            }
+            return  sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
